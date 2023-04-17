579f51fbf51a60bc67c3e39e3917c0c03e08e5f9
tush87 <tusharj8788@gmail.com>
2023-04-17 12:34:16 +0530

    add view single task routes

--- controller/task_controller.js
+++ controller/task_controller.js
@@ -1,16 +1,34 @@
 const TaskDB = require('../models/Task');
 
+//get all task
 module.exports.getAllTasks = async function(req,res){
     try{
         let allTasks = await TaskDB.find({});
         return res.status(200).json({
-            message:"done",
+            message:"All Tasks",
             allTasks
         })
     }
     catch(err){
         return res.status(400).json({
             message:"error"
+        })
+    }
+}
+
+//view task
+module.exports.viewTask = async function(req,res){
+    try{
+        let Task = await TaskDB.findById(req.params.id);
+        return res.status(200).json({
+            message:"Task",
+            Task
+        })
+    }
+    catch(err){
+        console.log(err);
+        return res.status(500).json({
+            message:"Internal Server Error"
         })
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const taskController = require('../controller/task_controller');
 const router = express.Router();
 
+//all task
 router.get('/task',taskController.getAllTasks);
+
+//get single task
+router.get('/task/:id',taskController.viewTask);
 
 module.exports=router;
