994ed0d2a7d1375d47f08fc73b146d67238a1c24
tush87 <tusharj8788@gmail.com>
2023-04-17 11:59:47 +0530

    add basic setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const port = 8000;
+
+const app = express();
+
+app.use(express.urlencoded({extended:false}));
+
+
+
+app.listen(port,(err)=>{
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log(`Server is up on port : ${port}`)
+});
