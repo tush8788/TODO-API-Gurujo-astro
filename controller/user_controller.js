9fde2577b303348ac14c28419a2fcf079085aec2
tush87 <tusharj8788@gmail.com>
2023-04-20 15:35:47 +0530

    add createsession

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -1,7 +1,9 @@
 const UserDB = require('../models/User');
-
+const jsonwebtoken = require('jsonwebtoken');
+//create user
 module.exports.createUser = async function(req,res){
     try{
+        // console.log(req.body)
         if(req.body.password != req.body.confirmpassword){
             return res.status(401).json({
                 message: "Password not match",
@@ -26,5 +28,28 @@ module.exports.createUser = async function(req,res){
         return res.status(400).json({
             message: "error"
         })
+    }
+}
+
+//create session 
+module.exports.createSession=async function(req,res){
+    try{
+        let user = await UserDB.findOne({email:req.body.email});
+        if(!user || user.password != req.body.password){
+            return res.status(402).json({
+                message:"email or password not match"
+            });
+        }
+
+        return res.status(200).json({
+            message:"user successfully login",
+            Token:jsonwebtoken.sign(user.toJSON(),"secret",{expiresIn:1000*60*100})
+        })
+    }
+    catch(err){
+        console.log(err);
+        return res.status(500).json({
+            message:"email or password not match"
+        });
     }
 }
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,10 @@ const express = require('express');
 const userController = require('../controller/user_controller');
 const router = express.Router();
 
+//create user 
 router.post('/create',userController.createUser);
+
+//create session
+router.post('/createsession',userController.createSession);
 
 module.exports=router;
